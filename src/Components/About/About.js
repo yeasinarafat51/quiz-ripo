17d96608718375af8eb34c1e611c2d8b78dddfce
yeasinarafat51 <aloondokar65@gmail.com>
2022-10-12 12:33:02 +0600

    second

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,7 @@ import './About.css'
 const About = () => {
     return (
         <div className='about'>
-            <h1>1.what is parpas of react router</h1>
+            <h1>1.what  parpas of react router</h1>
             <p> React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
             <h1>2. how does context api work</h1>
             <p>Answer:The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
